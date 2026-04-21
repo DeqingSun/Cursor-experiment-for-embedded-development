f483f52b18bd087dfe15483244a537430e8766de
Deqing Sun <iamseer@gmail.com>
2026-04-20 21:45:04 -0400

    try jump_isprom_strip

--- exp4_ch572_bootloader_reverse_engineer/ch572_simple_pure_makefile/main.cpp
+++ exp4_ch572_bootloader_reverse_engineer/ch572_simple_pure_makefile/main.cpp
@@ -1,6 +1,29 @@
 /* CH572: blink PA11. Single translation unit, no #include (registers + delay only).
  * PA11 digital GPIO, push-pull output (matches GPIOA_ModeCfg GPIO_ModeOut_PP_5mA).
  */
+ #include <stdint.h>
+
+ static void jump_isprom_strip()
+ {
+     volatile uint8_t *dst = (volatile uint8_t *)0x20000000;
+     const uint8_t *src = (const uint8_t *)(0x0003c000 + 0xc0);
+     for (uint32_t index = 0; index < 0x2000; ++index) {
+         dst[index] = src[index];
+     }
+ 
+     *(int32_t*)(0x20000100 + 0xc) = 0x00014505; // nop \n li a0,1, patch PA1 detection
+     
+     for (uint32_t index = 0; index < 0x0474; ++index) {
+         ((volatile uint8_t *)0x20001ba0)[index] = 0;
+     }
+ 
+     asm( "la gp, 0x20002398\n"
+          ".option arch, +zicsr\n"
+          "li t0, 0x20001196\n"
+          "csrw mepc, t0\n" // __set_MEPC is not available here
+          "mret\n");
+ 
+ }
 
 static void delay_cycles(volatile unsigned long n)
 {
@@ -21,6 +44,8 @@ int main(void)
         *((unsigned long*)(0x400010ACu)) |= (1<<11); //PA_CLR |= (1<<11);
         delay_cycles(10000ul);
     }
+
+    jump_isprom_strip();
 
     while (1) {
         ;
