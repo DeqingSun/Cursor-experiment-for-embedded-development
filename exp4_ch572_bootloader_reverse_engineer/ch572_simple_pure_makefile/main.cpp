26b861774d515b4b82aff4c258d0fe5b123836ab
Deqing Sun <iamseer@gmail.com>
2026-04-22 08:59:07 -0400

    add interrupt

--- exp4_ch572_bootloader_reverse_engineer/ch572_simple_pure_makefile/main.cpp
+++ exp4_ch572_bootloader_reverse_engineer/ch572_simple_pure_makefile/main.cpp
@@ -34,6 +34,42 @@ static void jump_isprom_strip()
     }
 }
 
+extern "C" {
+    void TMR_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast"))) __attribute__((section(".highcode")));
+    void TMR_IRQHandler(void) {
+        static bool level = false;
+        static int counter = 0;
+        static int toggleCount = 0;
+        counter++;
+        if (counter >= 10) { // toggle every 10 timer interrupts, about 100ms with current timer settings
+          level = !level;
+          counter = 0;
+          toggleCount++;
+          if (toggleCount == 20) { // after 2 seconds, jump to ISP
+            *((unsigned long*)(0xE000E180u)) = (1 << ((uint32_t)(24) & 0x1F)); // PFIC_DisableIRQ(TMR_IRQn);
+            asm volatile("fence.i");
+            //PFIC_DisableIRQ(TMR_IRQn);
+            *((unsigned long*)(0x40002402u)) = 0; // TMR_INTER_EN = 0;
+            //R8_TMR_INTER_EN = 0;
+            jump_isprom_strip();
+          }
+        }
+        if (level) {
+            *((unsigned long*)(0x400010A8u)) |= (1<<11); //PA_OUT |= (1<<11);
+        } else {
+            *((unsigned long*)(0x400010ACu)) |= (1<<11); //PA_CLR |= (1<<11);
+        }
+
+  
+
+        // PFIC_DisableIRQ(TMR_IRQn);
+        // *((unsigned long*)(0x40002402u)) = 0; // TMR_INTER_EN = 0;
+        // jump_isprom_strip();
+
+        *((unsigned long*)(0x40002406u)) = 0x01; // TMR_INT_FLAG = RB_TMR_IF_CYC_END;
+    }
+}
+
 static void delay_cycles(volatile unsigned long n)
 {
     while (n) {
@@ -47,14 +83,20 @@ int main(void)
     *((unsigned long*)(0x400010B4u)) &= ~(1<<11);  //PA_PD_DRV &= ~(1<<11);
     *((unsigned long*)(0x400010A0u)) |= (1<<11); //PA_DIR |= (1<<11);
 
-    for (int i=0;i<10;i++) {
-        *((unsigned long*)(0x400010A8u)) |= (1<<11); //PA_OUT |= (1<<11);
-        delay_cycles(10000ul);
-        *((unsigned long*)(0x400010ACu)) |= (1<<11); //PA_CLR |= (1<<11);
-        delay_cycles(10000ul);
-    }
+    *((unsigned long*)(0x4000240Cu)) = 60000; // TMR_CNT_END = 60000;
+    *((unsigned long*)(0x40002400u)) = 1<<1; // TMR_CTRL_MOD = RB_TMR_ALL_CLEAR;
+    *((unsigned long*)(0x40002400u)) = 1<<2; // TMR_CTRL_MOD = RB_TMR_COUNT_EN;
+    *((unsigned long*)(0x40002402u)) = 0x01; // TMR_INTER_EN = RB_TMR_IE_CYC_END;
+    *((unsigned long*)(0xE000E100u)) = (1 << ((uint32_t)(24) & 0x1F)); // PFIC_IENR1, PFIC_EnableIRQ(TMR_IRQn);
 
-    jump_isprom_strip();
+    // for (int i=0;i<10;i++) {
+    //     *((unsigned long*)(0x400010A8u)) |= (1<<11); //PA_OUT |= (1<<11);
+    //     delay_cycles(10000ul);
+    //     *((unsigned long*)(0x400010ACu)) |= (1<<11); //PA_CLR |= (1<<11);
+    //     delay_cycles(10000ul);
+    // }
+
+    // jump_isprom_strip();
 
     while (1) {
         ;
