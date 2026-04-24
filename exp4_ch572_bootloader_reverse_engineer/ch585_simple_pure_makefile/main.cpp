344a44f3bbc4baefdefd4541eca00e6c6644157c
Deqing Sun <iamseer@gmail.com>
2026-04-24 00:21:07 -0400

    bootloader works

--- exp4_ch572_bootloader_reverse_engineer/ch585_simple_pure_makefile/main.cpp
+++ exp4_ch572_bootloader_reverse_engineer/ch585_simple_pure_makefile/main.cpp
@@ -20,25 +20,27 @@ void highcode_init(void)
 
 __attribute__((noinline, used)) static void jump_isprom_strip()
 {
-    if (*((const uint32_t *)(0x0003c000 + 0xc0 + 0x10c)) == 0x00153513) { //    ram:0003c1cc 13 35 15 00     sltiu      a0,a0,0x1
+    if (*((const uint16_t *)(0x00078000 + 0x88 + 0x10a + 0xa)) == 0x8905) { //    0007819c: 0589 c.andi a0,0x1
         /* Word copy like WCH FLASH_ROM_READ: flash ROM reads are naturally 32-bit. */
         volatile uint32_t *dst = (volatile uint32_t *)0x20000000;
-        const uint32_t *src = (const uint32_t *)(0x0003c000 + 0xc0);
-        const uint32_t words = 0x2000u >> 2;
+        const uint32_t *src = (const uint32_t *)(0x00078000 + 0x88);
+        const uint32_t words = 0x2500u >> 2;
         for (uint32_t i = 0; i < words; ++i) {
             dst[i] = src[i];
         }
 
-        *(int32_t*)(0x20000100 + 0xc) = 0x00014505; // nop \n li a0,1, patch PA1 detection
+        //0x78192 is ReadPB11. 0007819c: 0589 c.andi a0,0x1
+        *(int16_t*)(0x2000010a + 0xa) = 0x4505; //li a0,1, patch PB11 (option byte is not read correctly) detection
         
-        volatile uint32_t *clr = (volatile uint32_t *)0x20001ba0;
-        for (uint32_t i = 0; i < (0x0474u >> 2); ++i) {
+        volatile uint32_t *clr = (volatile uint32_t *)0x200024f0;
+        for (uint32_t i = 0; i < (0x0560u >> 2); ++i) {
             clr[i] = 0;
         }
 
-        asm( "la gp, 0x20002398\n"
+        //0x200018c2 -> 0x7994A, main of bootloader 
+        asm( "la gp, 0x20002ce8\n"
             ".option arch, +zicsr\n"
-            "li t0, 0x20001196\n"
+            "li t0, 0x200018c2\n"
             "jr t0\n");
         //   "csrw mepc, t0\n" // __set_MEPC is not available here
         //   "mret\n");
@@ -107,7 +109,7 @@ int main(void)
         delay_cycles(10000ul);
     }
 
-    // jump_isprom_strip();
+    jump_isprom_strip();
 
     while (1) {
         ;
